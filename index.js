15fc1095cf19372db587456a5fb3bb6eaefd7256
S  1 <lokohopo@gmail.com>
2020-11-12 17:08:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string);
   return ("I can't hear you!")
   
-  if (string.toUpperCase() === uppercase);
+  if (string.toUpperCase() === string);
   return ("YES INDEED!")
   
   return ("I love you, too.")
