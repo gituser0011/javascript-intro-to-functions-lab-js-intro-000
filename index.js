01e83931cc71cc722b8352cae7d43deb5407c96b
S  1 <lokohopo@gmail.com>
2020-11-12 17:11:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function shout(string) {
-  return string.toUpperCase ()
+  return string.toUpperCase ();
 }
 
 function whisper(string) {
-  return string.toLowerCase ()
+  return string.toLowerCase ();
 }
 
 function logShout(string) {
@@ -16,10 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string);
-  return ("I can't hear you!")
+  return ("I can't hear you!");
   
   if (string.toUpperCase() === string);
-  return ("YES INDEED!")
+  return ("YES INDEED!");
   
   return ("I love you, too.")
 }
